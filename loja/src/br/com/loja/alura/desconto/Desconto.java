e4340cb41157b5e045cefd3f19bde64f53eec1de
DESKTOP-0GPQONJ\fabio <fabiosouza.web@gmail.com>
2021-11-19 22:27:47 -0300

    Feito template method

--- loja/src/br/com/loja/alura/desconto/Desconto.java
+++ loja/src/br/com/loja/alura/desconto/Desconto.java
@@ -11,6 +11,15 @@ public abstract class Desconto {
 	public Desconto(Desconto proximo) {
 		this.proximo = proximo;
 	}
-	
-	public abstract BigDecimal calcular(Orcamento orcamento);
+
+	public BigDecimal calcular(Orcamento orcamento){
+		if(deveAplicar(orcamento)){
+			return efetuarCalculo(orcamento);
+		}
+
+		return proximo.calcular(orcamento);
+	}
+
+	protected abstract BigDecimal efetuarCalculo(Orcamento orcamento);
+	public abstract boolean deveAplicar(Orcamento orcamento);
 }
